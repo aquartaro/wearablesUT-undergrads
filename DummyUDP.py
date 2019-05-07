0d12f92eeb05ab827b0ab8d7fefb0e8d1a796d4d
Amy Quartaro <amquartaro@gmail.com>
2019-05-07 12:44:46 -0500

    Get boolean output from data

--- DummyUDP.py
+++ DummyUDP.py
@@ -1,10 +1,17 @@
 import socket
 
+#Run me on a separate computer to simulate dongle
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(("127.0.0.1", 12345))
 
 while True:
-    message, address = server_socket.recvfrom(1024)
-    message = "\x00\x00\x00\x00,ffff\x00\x00\x00\xc3\n\x92\x99A \xd8zA~\x9c\xc6AgU\xd3"
+    try:
+        message, address = server_socket.recvfrom(1024)
+        message = "[12, 12, 3, 4]"
 
-    server_socket.sendto(message, address)
+        server_socket.sendto(message, address)
+    except KeyboardInterrupt:
+      # quit
+      print('the control c')
+      s.close()
+      sys.exit(0)
--- OpenBCI_reader.py
+++ OpenBCI_reader.py
@@ -1,5 +1,10 @@
 import socket
 import binascii
+# import matplotlib.pyplot as plt
+import numpy as np
+from time import sleep
+
+#OPTING NOT TO MESS WITH PLOTTING RIGHT NOW IN FAVOR OF USING TKINTER GUI
 
 print("i tried")
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -10,11 +15,21 @@ host = "127.0.0.1"
 port = 12345
 s.bind((host,port))
 
+# 0 for data stream
+# 1 for rand testing
+test = 1
+
+#threshold for ON ::: Need to test for this
+th = 0.5
+x = np.array([1,1])
+y = np.array([0,0])
+# fig = plt.figure()
 
 #Main loop for when Server Open!
 while True:
    try:
-        try:
+        if test == 0:
+          try:
             data, addr = s.recvfrom(1024)
             #print ('Got connection from', addr)
             print ("the data is below:")
@@ -29,8 +44,27 @@ while True:
             vals = decode_all[1].split(',')
             print(vals)
             #print (vals[0], '|',vals[1])
-        except socket.timeout:
+          except socket.timeout:
             print('REQUEST TIMED OUT')
+        elif test == 1: #move the stuff in here to time out as well
+          vals = [0.1,0,0,0]
+          print(x ,' and ', y)
+
+        y = np.append(y,vals[0])
+        x = np.append(x,[vals[1]])
+
+        if np.mean(y[-10::]) >= th:
+          print("Above threshold")
+          muscle = True
+        else:
+          print("did not meet conditions")
+          muscle = False
+
+
+        sleep(1)  
+
+
+
 
         #print("runnin'")
      
@@ -43,5 +77,7 @@ while True:
       print('the control c')
       s.close()
       sys.exit(0)
+
+
 
    
